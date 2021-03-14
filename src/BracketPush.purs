a67073722d5fb0d9549faf1394889a9adca00587
LeaveNhA <seckin.kukrer@hotmail.com>
2021-03-14 22:08:48 +0300

    Develop the solution.

--- src/BracketPush.purs
+++ src/BracketPush.purs
@@ -1,30 +1,33 @@
 module BracketPush where
 
-import Data.Array (foldl)
-import Data.Maybe (Maybe(..))
-import Data.Stack (Stack, stackIsEmpty, stackNew, stackPop, stackPush)
+import Data.Array (foldl, snoc, null, last, tail, singleton, fromFoldable, take, length, init)
+import Data.Maybe (Maybe(..), fromJust)
 import Data.String.CodeUnits (toCharArray)
 import Data.Tuple (Tuple(..))
-import Prelude (($), (==), (||))
+import Prelude (($), (==), (||), (/), (+), (-))
 
-stackBrackets :: Maybe (Stack Char) -> Char -> Maybe (Stack Char)
+bracketToInt :: Char -> Maybe Int
+bracketToInt '(' = Just 1
+bracketToInt '[' = Just 2
+bracketToInt '{' = Just 3
+bracketToInt ')' = Just 4
+bracketToInt ']' = Just 5
+bracketToInt '}' = Just 6
+bracketToInt _ = Nothing
+
+stackBrackets :: Maybe (Array Int) -> Char -> Maybe (Array Int)
 stackBrackets Nothing _ = Nothing
-stackBrackets (Just stack) c
-  | c == '[' || c == '(' || c == '{' = Just $ stackPush stack c
-stackBrackets (Just stack) c =
-  case c of
-    ']' -> case (stackPop stack) of
-      Just (Tuple poppedStack nc) -> if '[' == nc then Just $ poppedStack else Nothing
-      Nothing -> Nothing
-    '}' -> case (stackPop stack) of
-      Just (Tuple poppedStack nc) -> if '{' == nc then Just $ poppedStack else Nothing
-      Nothing -> Nothing
-    ')' -> case (stackPop stack) of
-      Just (Tuple poppedStack nc) -> if '(' == nc then Just $ poppedStack else Nothing
-      Nothing -> Nothing
-    otherwise -> Just stack
+stackBrackets (Just brackets) c
+  | c == '[' || c == '(' || c == '{' = case (bracketToInt c) of
+    Just bracketInt -> Just $ snoc brackets bracketInt
+    Nothing -> Nothing
+stackBrackets (Just brackets) c =
+  case { l: last brackets, bi: (bracketToInt c) } of
+    {l: _, bi: Nothing} -> Just brackets
+    {l: Just lastBracket, bi: Just bracketInteger} -> if (lastBracket + 3) == bracketInteger then init brackets else Nothing
+    _ -> Nothing
 
 isPaired :: String -> Boolean
-isPaired pairs = case foldl stackBrackets (Just (stackNew :: Stack Char)) $ toCharArray pairs of
-  Just stack -> stackIsEmpty stack
+isPaired pairs = case foldl stackBrackets (Just ([] :: Array Int)) $ toCharArray pairs of
+  Just brackets -> null brackets
   Nothing -> false
